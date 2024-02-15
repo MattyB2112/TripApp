d1e3db9110b568f05c899bb79ec3d652c9502689
Matt Barker <mattbarker23@hotmail.com>
2024-02-15 12:32:59 +0000

    test push

--- App.js
+++ App.js
@@ -15,11 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
     color: "#fff",
+    textAlign: "center",
   },
 });
