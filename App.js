7245f945d171274ceb0daa3fcf2db29689e8483d
stavros <polik_as@hotmail.com>
2024-02-15 15:09:50 +0000

    conflict incoming

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
       <Text style={styles.text}>Hello everyone!</Text>
       <Text style={styles.text}>Lala wrote this.</Text>
       <Text style={styles.text}>It's been a long time...</Text>
+      <Text style={styles.text}>Stavros text...</Text>
       <StatusBar style="auto" />
     </View>
   );
