2dadc799a5faafb460ea0d34d9e40d648c648d1d
lalauo <luwa.oye@gmail.com>
2024-02-23 15:58:33 +0000

    implemented trip adding functionality

--- components/Trip/TripAdder.jsx
+++ components/Trip/TripAdder.jsx
@@ -4,111 +4,175 @@ import {
   Text,
   StyleSheet,
   TextInput,
-  KeyboardAvoidingView,
+  SafeAreaView,
   ScrollView,
   Pressable,
 } from "react-native";
 import DatePicker from "@dietime/react-native-date-picker";
 
 export default function TripAdder() {
+  const [tripName, setTripName] = useState("");
+
+  const [startPicker, setStartPicker] = useState(false);
+  const [endPicker, setEndPicker] = useState(false);
+
+  const [date, setDate] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
   const [newTrip, setNewTrip] = useState("");
 
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-
-  const [showPicker, setShowPicker] = useState(false);
-
   const loggedInUser = "Jenny";
+  const currentYear = new Date().getFullYear();
+
+  function handleTripNameInput(text) {
+    setTripName(text);
+  }
+
+  function handleFromPress() {
+    setStartPicker(true);
+  }
+
+  function handleSetStartDate() {
+    setStartPicker(false);
+    const dateStr = new Date(date).toISOString();
+    setStartDate(dateStr);
+  }
+
+  function handleToPress() {
+    setEndPicker(true);
+  }
+
+  function handleSetEndDate() {
+    setEndPicker(false);
+    const dateStr = new Date(date).toISOString();
+    setEndDate(dateStr);
+  }
+
+  function formatDate(date) {
+    return new Date(date).toDateString();
+  }
 
   const tripToAdd = {
-    _id: "8549495e9434352fg234",
-    name: "Bogota",
-    startdate: "2024-04-26T12:30:59.657+00:00",
-    enddate: "2024-05-27T21:00:59.657+00:00",
+    name: tripName,
+    startdate: startDate,
+    enddate: endDate,
     admin: loggedInUser,
   };
 
-  const currentYear = new Date().getFullYear();
-
-  function handlePress() {
-    setNewTrip(newTrip);
-    console.log(newTrip);
-  }
-
   return (
-    <ScrollView style={styles.container} behavior="padding">
-      <KeyboardAvoidingView>
-        <View>
-          <Text style={styles.inputHeader}>Trip Name</Text>
-          <TextInput style={styles.textInput} />
-        </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {/* Trip Name Input */}
+        <Text>TRIP NAME:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={handleTripNameInput}
+        />
 
-        <View>
-          <Pressable onPress={setShowPicker}>
-            <Text style={styles.inputHeader}> Set Start Date</Text>
-          </Pressable>
+        {/* Start Date Picker */}
+        <Pressable onPress={handleFromPress}>
+          <Text>FROM: {startDate ? formatDate(startDate) : ""}</Text>
+        </Pressable>
 
-          {showPicker && (
+        {startPicker && (
+          <View>
             <DatePicker
-              value={startDate}
-              onChange={(value) => setStartDate(value)}
+              value={date}
+              onChange={(value) => setDate(value)}
               format="dd-mm-yyyy"
               startYear={currentYear}
               endYear={currentYear + 50}
               fontSize={15}
             />
-          )}
-          {/* <Pressable>
-            {" "}
-            <Text>Confirm</Text>{" "}
-          </Pressable> */}
-        </View>
 
-        <View>
-          <Pressable onPress={setShowPicker}>
-            <Text style={styles.inputHeader}> Set End Date</Text>
-          </Pressable>
+            <Pressable onPress={handleSetStartDate} style={styles.setDateBtn}>
+              <Text style={styles.setDateText}>SET START DATE</Text>
+            </Pressable>
+          </View>
+        )}
+        {/* End Date Picker */}
+        <Pressable onPress={handleToPress}>
+          <Text>TO: {endDate ? formatDate(endDate) : ""}</Text>
+        </Pressable>
 
-          {showPicker && (
+        {endPicker && (
+          <View>
             <DatePicker
-              value={endDate}
-              onChange={(value) => setEndDate(value)}
+              value={date}
+              onChange={(value) => setDate(value)}
               format="dd-mm-yyyy"
               startYear={currentYear}
               endYear={currentYear + 50}
               fontSize={15}
             />
-          )}
-          {/* <Pressable>
-            {" "}
-            <Text>Confirm</Text>{" "}
-          </Pressable> */}
-        </View>
-        <View>
-          <Pressable onPress={handlePress}>
-            {" "}
-            <Text>Create Trip</Text>{" "}
+
+            <Pressable onPress={handleSetEndDate} style={styles.setDateBtn}>
+              <Text style={styles.setDateText}>SET END DATE</Text>
+            </Pressable>
+          </View>
+        )}
+
+        {/* Create Trip Button */}
+        {startDate && endDate ? (
+          <Pressable
+            style={styles.createTripBtn}
+            onPress={() => setNewTrip(tripToAdd)}
+          >
+            <Text style={styles.createTripText}>CREATE TRIP!</Text>
           </Pressable>
-        </View>
-      </KeyboardAvoidingView>
-    </ScrollView>
+        ) : (
+          ""
+        )}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  inputHeader: {
-    fontWeight: "bold",
-  },
   container: {
     flex: 1,
     padding: 50,
     backgroundColor: "#D7CCB2",
   },
   textInput: {
-    backgroundColor: "#D7CCB2",
+    backgroundColor: "white",
     color: "black",
     borderColor: "#423219",
     borderWidth: 2,
     borderRadius: 5,
+    padding: 5,
+    width: "80%",
+    alignSelf: "center",
+  },
+  setDateBtn: {
+    alignSelf: "center",
+    alignItems: "center",
+    width: "30%",
+    borderColor: "#423219",
+    borderWidth: 2,
+    borderRadius: 5,
+    paddingVertical: 10,
+    marginVertical: 5,
+  },
+  setDateText: {
+    fontWeight: "bold",
+    fontSize: 10,
+  },
+  createTripBtn: {
+    backgroundColor: "#423219",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "30%",
+    borderColor: "#291E0E",
+    borderWidth: 3,
+    borderRadius: 5,
+    paddingVertical: 10,
+    marginVertical: 5,
+  },
+  createTripText: {
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "#096502",
   },
 });
